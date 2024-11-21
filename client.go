a5065bf70ce4ba41349997350525f9b0d05b1fe1
Подмосковный Александр <podmoskovniy@ap.com>
2024-11-21 15:33:52 +0300

    add post request to regru v1.1.9

--- client.go
+++ client.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"time"
+	regruapi "github.com/daloman/regru-api-go/zonecontrol"
+	"github.com/sirupsen/logrus"
 )
-
-const (
-	defaultBaseURL = "https://api.reg.ru/api/regru2"
-	httpTimeout    = 10 * time.Second
-)
-
-var client = &http.Client{Timeout: httpTimeout}
 
 type RegruClient struct {
 	username string
@@ -31,92 +20,23 @@ func NewRegruClient(username string, password string, zone string) *RegruClient 
 	}
 }
 
-// sendRequest отправляет POST-запрос с параметрами и возвращает ответ
-func (c *RegruClient) sendRequest(method string, params map[string]interface{}) ([]byte, error) {
-	url := defaultBaseURL + "/" + method
-	params["username"] = c.username
-	params["password"] = c.password
-
-	// Преобразуем параметры в JSON
-	body, err := json.Marshal(params)
-	if err != nil {
-		return nil, fmt.Errorf("не удалось преобразовать данные в JSON: %v", err)
-	}
-
-	// Создаем POST-запрос
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
-	if err != nil {
-		return nil, fmt.Errorf("не удалось создать запрос: %v", err)
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-
-	// Выполняем запрос
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("не удалось выполнить запрос: %v", err)
-	}
-	defer resp.Body.Close()
-
-	// Читаем ответ
-	respBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("не удалось прочитать ответ: %v", err)
-	}
-
-	// Логируем ответ API
-	fmt.Println("Ответ API:", string(respBody))
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("ошибка API: %s", string(respBody))
-	}
-
-	return respBody, nil
-}
-
 // CreateTXT создает TXT запись для домена
 func (c *RegruClient) createTXT(domain string, value string) error {
-	params := map[string]interface{}{
-		"zone":   c.zone,
-		"domain": domain,
-		"txt":    value,
-	}
-
-	_, err := c.sendRequest("zone/add_txt", params)
-	if err != nil {
-		return fmt.Errorf("createTXT: API вернуло ошибку: %v", err)
-	}
-
+	// Используем метод для добавления TXT записи
+	logrus.Debugf("Add %v TXT resource record for %v domain with the following content %v", domain, c.zone, value)
+	regruapi.AddTxtRr(c.username, c.password, c.zone, domain, value)
 	return nil
 }
 
 // DeleteTXT удаляет TXT запись для домена
 func (c *RegruClient) deleteTXT(domain string, value string) error {
-	params := map[string]interface{}{
-		"zone":   c.zone,
-		"domain": domain,
-		"txt":    value,
-	}
-
-	_, err := c.sendRequest("zone/remove_record", params)
-	if err != nil {
-		return fmt.Errorf("deleteTXT: API вернуло ошибку: %v", err)
-	}
-
+	logrus.Debugf("Add %v TXT resource record for %v domain with the following content %v", domain, c.zone, value)
+	regruapi.RmTxtRr(c.username, c.password, c.zone, domain, "TXT", value)
 	return nil
 }
 
 // getRecords получает TXT записи для зоны
-func (c *RegruClient) getRecords() ([]byte, error) {
-	params := map[string]interface{}{
-		"zone": c.zone,
-	}
-
-	// Запрашиваем записи для зоны
-	respBody, err := c.sendRequest("zone/get_resource_records", params)
-	if err != nil {
-		return nil, fmt.Errorf("getRecords: ошибка при получении записей: %v", err)
-	}
-
-	return respBody, nil
+func (c *RegruClient) getRecords() error {
+	regruapi.GetZones(c.username, c.password, c.zone)
+	return nil
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ go 1.19
 
 require (
 	github.com/cert-manager/cert-manager v1.11.0
+	github.com/daloman/regru-api-go v0.1.0-alpha.1
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.9.0
 	k8s.io/client-go v0.26.1
 	k8s.io/klog/v2 v2.80.1
 )
@@ -47,7 +50,6 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_golang v1.14.0 // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common v0.37.0 // indirect
